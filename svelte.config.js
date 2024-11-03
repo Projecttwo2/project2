e929a61acc0232ee053568046a314f8cd2fabc30
Wendell05162003 <157675829+Wendell05162003@users.noreply.github.com>
2024-11-03 23:06:14 +0800

    First Commit

---
+++ svelte.config.js
@@ -0,0 +1,30 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null, // Adjust this if needed
+      precompress: false
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/project2' : ''
+    },
+    prerender: {
+      handleHttpError: ({ status, path, referrer, referenceType }) => {
+        if (status === 404) {
+          // Ignore 404 errors on specific routes
+          return;
+        }
+        // Throw an error for other statuses
+        throw new Error(`${status} ${path}`);
+      }
+    }
+  }
+};
+
+export default config;
