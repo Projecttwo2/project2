ee1d3f12b4c9f442aba04ad0fe341b3bdb2ab62f
Wendell05162003 <157675829+Wendell05162003@users.noreply.github.com>
2024-11-04 10:56:09 +0800

    project2 commit

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -7,10 +7,10 @@
 <header>
     <div class="logo"></div>
     <nav>
-        <a href="/">Home</a>
-        <a href="/shop">Shop</a>
-        <a href="/contact">Contact</a>
-    </nav>
+        <a href="/project2/">Home</a>
+        <a href="/project2/shop">Shop</a>
+        <a href="/project2/contact">Contact</a>
+      </nav>
 </header>
   
 <main>
